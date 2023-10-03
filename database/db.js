ee479a631ca01ed9846a5ebb6741a9b42c0ba284
shanachan <77118749+kap320@users.noreply.github.com>
2023-10-03 13:57:41 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sorabot.xyz';
+var url = 'https://api-shanachanofc.cyclic.cloud';
 try {
-if(url == 'https://api.sorabot.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api-shanachanofc.cyclic.cloud') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
